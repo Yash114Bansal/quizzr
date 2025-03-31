f53ca6e27f6cc3669066324d3819df1946c66b2e
Yash Bansal <yash114bansal@gmail.com>
2025-03-31 14:40:03 +0530

    ADD: create modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-"use client"
 import Counter from "@/components/Counter";
 import quizzrs from "@/components/Home/quizzrs";
+import Createquizzr from "@/components/Home/Createquizzr";
 
 export default function Home() {
   return (
-    <>
+    <div>
       <quizzrs />
+      <Createquizzr/>
       <Counter />
-    </>
+    </div>
   );
 }
