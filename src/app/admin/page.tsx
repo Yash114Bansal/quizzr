40ab4780e90019d9a9bf77f127eecc8d0d118015
Yash Bansal <yash114bansal@gmail.com>
2025-06-05 14:45:05 +0530

    UPDATE: admin homepage

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,14 +1,24 @@
 import quizzrs from "@/components/Admin/Home/quizzrs";
-import Modal from "@/components/Modal";
-import CreatequizzrForm from "@/components/Admin/Home/CreatequizzrForm";
+// import Modal from "@/components/Modal";
+// import CreatequizzrForm from "@/components/Admin/Home/CreatequizzrForm";
+import NavbarToggle from "@/components/Admin/NavbarToggle";
 
 export default function Home() {
   return (
-    <div className="text-slate-200">
+    <div className="p-6 w-full md:w-[75%]">
+      <div className="flex">
+        <span className="md:hidden inline">
+          <NavbarToggle />
+        </span>
+        <span className="ml-2">
+          <p className="dark:text-white text-xs md:text-base">Hey There 👋!</p>
+          <h1 className="text-md md:text-3xl font-black md:py-2 dark:text-white">Welcome Back To Your Quiz Hub!</h1>
+        </span>
+      </div>
       <quizzrs />
-      <Modal btnTitle="Create quizzr">
+      {/* <Modal btnTitle="Create quizzr">
         <CreatequizzrForm />
-      </Modal>
+      </Modal> */}
     </div>
   );
 }
